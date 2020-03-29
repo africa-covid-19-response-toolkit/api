801a130efd9062e69b814eb928ff3c1b9949c75c
Abel Beyene <contact.abeyene@gmail.com>
2020-03-29 16:57:52 +0100

    [WIP] introduced filtering

--- handlers/get.js
+++ handlers/get.js
@@ -8,11 +8,12 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.get = (event, context, callback) => {
   const {
     pathParameters: { type, id },
+    queryStringParameters
   } = event;
 
   const table = getTable(type);
 
-  if (!table)
+  if (!table) {
     callback(null, {
       statusCode: 400,
       headers: {
@@ -21,6 +22,49 @@ module.exports.get = (event, context, callback) => {
       },
       body: { message: `Unknown type provided. Type name: ${type}` },
     });
+  }
+
+  if (queryStringParameters) {
+    const scanFilter = {};
+    Object.keys(queryStringParameters).forEach((key) => {
+      scanFilter[key] = {
+        ComparisonOperator: 'EQ',
+        AttributeValueList: [
+          queryStringParameters[key]
+        ]
+      }
+    });
+
+    const filterParams = {
+      TableName: table,
+      ScanFilter: scanFilter
+    }
+
+    // TODO: can we use something other than scan?
+    dynamoDb.scan(filterParams, (error, result) => {
+      // handle potential errors
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { 'Content-Type': 'text/plain' },
+          body: { message: `Couldn't fetch the ${type} item.` },
+        });
+        return;
+      }
+  
+      // create a response
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(result.Item || {}),
+      };
+      callback(null, response);
+    });
+  }
 
   const params = {
     TableName: table,
